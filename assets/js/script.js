e551c20d34e5788836d568139facb251f03cbe2d
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-08-16 15:47:57 -0600

    API calls fetch instead of ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,38 +23,29 @@ function next(data) {
   return tempData;
 }
 
-function getSpecificApi(endpoint, usa) {
-  // set endpoint and your access key
-  var endpoint = "2022-08-15"; //querySelector outside function
+function getCommodityBySymbol(symbol, currency, date ) {
   var access_key =
-    "ljdbuf72k16ob3i9jqscexucnfazsxi7l4deffx4d8w9ws8iyx7y0f2vp971";
-  var base = "&base=XAU";
-  var symbols = "&symbols=" + requestedSymbol;
-  var requestedSymbol = "USD"; //querySelector here outside
-  //   ,GBP,BRENTOIL,XAG,WHEAT,CORN
-  // get the most recent exchange rates via the "latest" endpoint:
-  $.ajax({
-    url:
-      "https://commodities-api.com/api/open-high-low-close/" +
-      endpoint +
-      "?access_key=" +
-      access_key +
-      base +
-      symbols,
+    //"ljdbuf72k16ob3i9jqscexucnfazsxi7l4deffx4d8w9ws8iyx7y0f2vp971"; // vian's key
+      "5j9z3tm51x3q548swpzl0chbh4o5html88lm1htqpcbmdkwtgzl7f5boy4r2"; // raymond's key
+  var base = "&base="+symbol;
+  var symbols = "&symbols=" + currency;
+  fetch("https://commodities-api.com/api/open-high-low-close/" +
+        date +
+        "?access_key=" +
+        access_key +
+        base +
+        symbols)
+  .then(function(request){
+    return request.json();
+    })
+  .then( function(request){
+    console.log('high', request.rates.high);
+    console.log('close',request.rates.close);
+    console.log('low',request.rates.low);
+    console.log('open',request.rates.open);
+    return request.rates;
+    })
 
-    dataType: "json",
-    success: function (json) {
-      // exchange rata data is stored in json.rates
-      console.log(json);
-      //   alert(json.rates.GBP);
-
-      //   // base currency is stored in json.base
-      //   alert(json.base);
-
-      //   // timestamp can be accessed in json.timestamp
-      //   alert(json.timestamp);
-    },
-  });
 }
 
 let stringTooSymbol = {
@@ -69,8 +60,10 @@ let stringTooSymbol = {
 $('#fetch-button').on('click',function(event){
   let dropDown = $('#dropDownTxt');
   let commSelect= $(dropDown[0]).text();
-  let currentcy = "USD";
-  console.log("Fetching "+commSelect ,"SYM : " + stringTooSymbol[commSelect] ," Currency: ",currentcy );
-  getSpecificApi();
+  let currency = "USD";
+  let today = new Date();
+  let todayString = today.getFullYear()+ '-' + today.getMonth() +'-'+today.getDate();
+  console.log("Fetching "+commSelect ,"SYM : " + stringTooSymbol[commSelect] ," Currency: ",currency );
+  getCommodityBySymbol(stringTooSymbol[commSelect],currency, todayString);
 });
 
